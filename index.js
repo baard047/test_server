c78d6365c3e6a860fa3cf69f4a3ffa573d1d2776
Alexander Bardashevsky <baard047@gmail.com>
2020-05-28 00:34:12 +0300

    Added body parser

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const express = require('express');
 //const {Router} = require('express')
 const app = express();
-//const router = Router();
 
-app.use(express.json({ extended: true }));
+const bodyParser = require('body-parser');
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     console.log('Входящий запрос: ', req.data);
-    res.send('Hello World from Bard-server!');
+    res.send('ты пидор!');
+});
+
+app.post('/', (req, res) => {
+    console.log('Входящий POST запрос: ', req.body);
+    res.send("transfer:\"11\"");
 });
 
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Сервер запущен на порту ${PORT}`);
 });
+
